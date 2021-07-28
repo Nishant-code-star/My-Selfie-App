e7f97d2e840b4771ecd05828a7869c1b97de448d
Nishant-code-star <79645199+Nishant-code-star@users.noreply.github.com>
2021-07-28 09:40:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,19 +11,38 @@ recognition.onresult = function run(event) {
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
-    speak();
+    if (content == "Take my selfie.") {
+        speak();
+    }
 }
 
-function speak(){
-    var synth=window.speechSynthesis;
-    data=document.getElementById("textbox").value;
-    var utter=new SpeechSynthesisUtterance(data);
+function speak() {
+    var synth = window.speechSynthesis;
+    data = "taking your selfie in 5 seconds";
+    var utter = new SpeechSynthesisUtterance(data);
     synth.speak(utter);
     Webcam.attach(camera);
+    setTimeout(function () {
+    take_snapshot();
+    save();
+    }, 5000);
 }
 
 Webcam.set({
-    width:360,height:250,image_format:'png',png_qaulity:90
+    width: 360, height: 250, image_format: 'png', png_qaulity: 90
 });
 
-camera=document.getElementById("camera");
+camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='selfie_image' src='" + data_uri + "'>";
+    });
+}
+
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src
+    link.href=image;
+    link.click()
+}
